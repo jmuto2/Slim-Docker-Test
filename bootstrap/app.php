d459d6998584ef8a111b02d1594f870dfc1c8e00
jmuto2 <jmuto2@gmail.com>
2017-06-06 20:44:04 -0400

    app config  setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,10 @@
 <?php
 
+define('DIR', __DIR__);
+
 session_start();
 
-require __DIR__ . '/../vendor/autoload.php';
-
+require DIR . '/../vendor/autoload.php';
 
 $app = new \Slim\App([
     'settings' => [
@@ -12,6 +13,24 @@ $app = new \Slim\App([
     ]
 ]);
 
-$app->get('/', function ($request, $response) {
-    return 'Home sweet home';
-});
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(DIR . '/../resources/views', [
+        'cache' => false,
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+
+    return $view;
+};
+
+$container['HomeController'] = function ($container) {
+    return new \App\Controllers\HomeController($container);
+};
+
+require DIR . '/../app/routes.php';
